e690a8a96eb5c40a9e32d43e65b1532600977f70
VanDucKun <havanducdeptrai@gmail.com>
2023-02-23 00:16:28 +0700

    add file for bai5

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodosController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,8 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::get('about', function () {
-//     return view('about');
-// });
 
-Route::get('about', 'App\Http\Controllers\AboutController@index');
+Route::get('/todos', [TodosController::class, 'index']);
