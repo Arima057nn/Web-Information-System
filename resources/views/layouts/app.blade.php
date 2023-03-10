608b56c91a42215cd3480ea08883e0574409c735
VanDucKun <havanducdeptrai@gmail.com>
2023-03-11 00:35:05 +0700

    bai 16

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,34 +2,54 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" crossorigin="anonymous">
-    <title>
-        @yield('title')
-    </title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" crossorigin="anonymous">
+  <title>
+    @yield('title')
+  </title>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">Todos App</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="/todos">Todos <span class="sr-only">(current)</span></a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/">Todos App</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-    <div class="container">
-        @yield('content')
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="/todos">Todos <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/new-todos">Create todos <span class="sr-only">(current)</span></a>
+        </li>
+      </ul>
     </div>
+  </nav>
+
+  <div class="container">
+    @if(session()->has('create-success'))
+    <div class="alert alert-success">
+      {{ session()->get('create-success') }}
+    </div>
+    @endif
+
+    @if(session()->has('update-success'))
+    <div class="alert alert-success">
+      {{ session()->get('update-success') }}
+    </div>
+    @endif
+
+    @if(session()->has('delete-success'))
+    <div class="alert alert-success">
+      {{ session()->get('delete-success') }}
+    </div>
+    @endif
+    @yield('content')
+  </div>
 </body>
 
 </html>
